581e58d1de631ec4060a1157fc24ae052bfe96b3
lovey-gautam <loveygautam55@gmail.com>
2026-04-22 14:49:17 +0530

    Update VerifyEmail.jsx

--- Frontend/src/pages/VerifyEmail.jsx
+++ Frontend/src/pages/VerifyEmail.jsx
@@ -7,7 +7,7 @@ const VerifyEmail = () => {
     const navigate = useNavigate()
     const VerifyEmail= async()=>{
         try{
-            const res = await axios.post(`${import.meta.env.VITE_URL}/user/verify`,{},{
+            const res = await axios.get(`${import.meta.env.VITE_URL}/user/verify`,{},{
                 headers:{
                     Authorization:`Bearer ${token}`
                 }
