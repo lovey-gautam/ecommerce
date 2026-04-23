72d0e68e04576673cb566e5b53a1709cdbf5a5d6
lovey-gautam <loveygautam55@gmail.com>
2026-04-23 20:51:27 +0530

    Update VerifyEmail.jsx

--- Frontend/src/pages/VerifyEmail.jsx
+++ Frontend/src/pages/VerifyEmail.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 
-const verifyEmail = () => {
+const VerifyEmail = () => {
     const {token} = useParams()
     const [status,setStatus] = useState("verifying...")
     const navigate = useNavigate()
-    const VerifyEmail= async()=>{
+    const verifyEmail= async()=>{
         try{
             const res = await axios.get(`${import.meta.env.VITE_URL}/user/verify-email/${token}`);
                
