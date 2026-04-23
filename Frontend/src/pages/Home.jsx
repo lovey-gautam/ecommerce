495b36a06bc80bef0496ba6400565970a0d1237d
lovey-gautam <loveygautam55@gmail.com>
2026-04-23 21:52:17 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,16 @@ import Hero from '@/components/Hero'
 import React from 'react'
 import Features from '@/components/Features'
 const Home = () => {
+   const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    // simulate API load OR wait for child components
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [])
+
+  if (loading)
   return (
     <div>
       <Hero/>
