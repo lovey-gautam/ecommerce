4cdbd6f6895e2fc1cdb24a1b21b41ade935b2691
lovey-gautam <loveygautam55@gmail.com>
2026-04-23 20:50:35 +0530

    Update VerifyEmail.jsx

--- Frontend/src/pages/VerifyEmail.jsx
+++ Frontend/src/pages/VerifyEmail.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 
-const VerifyEmail = () => {
+const verifyEmail = () => {
     const {token} = useParams()
     const [status,setStatus] = useState("verifying...")
     const navigate = useNavigate()
     const VerifyEmail= async()=>{
         try{
-            const res = await axios.get(`${import.meta.env.VITE_URL}/user/verify/${token}`);
+            const res = await axios.get(`${import.meta.env.VITE_URL}/user/verify-email/${token}`);
                
             if(res.data.success){
                 setStatus('✅Email Verified Successfully')
@@ -23,13 +23,13 @@ const VerifyEmail = () => {
         }
     }
     useEffect(()=>{
-        VerifyEmail()
+        verifyEmail()
     },[token])
   return (
     <div className='relative w-full h-[760] bg-pink-100 overflow-hidden' >
         <div className='min-h-screen flex items-center justify-center'>
             <div className='bg-white p-6 rounded-2xl shadow-md text-center w-[90%] maxw-md'>
-                <h2 className='text-xl font-semihold text-gray-800'>{status}</h2>
+                <h2 className='text-xl font-semibold text-gray-800'>{status}</h2>
             </div>
         </div>
       verify email
