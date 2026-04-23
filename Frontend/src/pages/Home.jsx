5692ebca184d20db81aa8e96eb3e91ac790f5eb9
lovey-gautam <loveygautam55@gmail.com>
2026-04-23 22:33:00 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -11,7 +11,13 @@ const Home = () => {
     }, 1000)
   }, [])
 
-  if (loading)
+   if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <div className="w-10 h-10 border-4 border-pink-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    )
+  }
   return (
     <div>
       <Hero/>
