14f69aecf2fa7c5806d2b6f79b4fd3d7f8574ec5
lovey-gautam <loveygautam55@gmail.com>
2026-04-23 09:29:31 +0530

    added vercel config for routing fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
