7e19b5b3244e2c72abd1c8545a01e6c3128c35d1
lovey-gautam <loveygautam55@gmail.com>
2026-04-26 10:40:54 +0530

    Update ProductImg.jsx

--- Frontend/src/components/ProductImg.jsx
+++ Frontend/src/components/ProductImg.jsx
@@ -17,22 +17,26 @@ const ProductImg = ({images=[]}) => {
 }, [images]);
   return (
     
-    <div className='flex gap-5 w-max'>
-      <div className='gap-5 flex flex-col'>{
+    <div className='flex flex-col md:flex-row gap-4 w-full'>
+      <div className='flex md:flex-col gap-3 overflow-x-auto md:overflow-visible'>{
         
         images.map((img,idx)=>{
          return <img onClick={()=>setMainImg(img.url)}
            key={idx}
            src={img.url} alt="" 
-           className='cursor-pointer w-20 border shadow-lg'/>
+           className='cursor-pointer w-16 h-16 object-cover border shadow-md flex-shrink-0'/>
         })
         }
       </div>
+      <div className = "w-full flex justify-center">
       <Zoom>
       
-       <img src={mainImg} alt="" className='w-[500px] border shadow-lg'/>
+       <img src={mainImg} alt="" 
+        className='w-full max-w-md md:max-w-lg object-contain border shadow-lg'
+/>
 
       </Zoom>
+      </div>
     </div>
   )
 }
