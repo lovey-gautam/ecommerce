7b542be56f206764b7a81ecf44436e443fc2ba82
lovey-gautam <loveygautam55@gmail.com>
2026-04-26 10:48:14 +0530

    Update Sidebar.jsx

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -6,25 +6,25 @@ import { PackageSearch } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
 const Sidebar = () => {
   return (
-    <div className='hidden fixed md:block border-r bg-pink-50 border-pink-200 x-10 w-[300px] p-10 space-y-2 h-screen'>
-        <div className='text-center pt-10 px-3 space-y-2'>
-            <NavLink to = '/dashboard/sales' className={({isActive})=> `text-xl ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
+    <div className='hidden md:block fixed top-0 left-0 h-screen w-64 bg-pink-50 border-r border-pink-200 p-6 overflow-y-auto'>
+        <div className=' pt-10  space-y-2'>
+            <NavLink to = '/dashboard/sales' className={({isActive})=> `text-lg ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
             flex items-center gap-2 font-bold cursor-pointer p-3 rounded-2xl w-full`}>
             <LayoutDashboard/><span>Dashboard</span></NavLink>
 
-            <NavLink to = '/dashboard/add-products' className={({isActive})=> `text-xl ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
+            <NavLink to = '/dashboard/add-products' className={({isActive})=> `text-lg ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
                 flex items-center gap-2 font-bold cursor-pointer p-3 rounded-2xl w-full`}>
             <PackagePlus/><span>Add Product</span></NavLink>
 
-            <NavLink to = '/dashboard/products' className={({isActive})=>  `text-xl ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
+            <NavLink to = '/dashboard/products' className={({isActive})=>  `text-lg ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
                 flex items-center gap-2 font-bold cursor-pointer p-3 rounded-2xl w-full`}>
             <PackageSearch/><span>Product</span></NavLink>
 
-             <NavLink to = '/dashboard/users' className={({isActive})=>  `text-xl ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
+             <NavLink to = '/dashboard/users' className={({isActive})=>  `text-lg ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
                 flex items-center gap-2 font-bold cursor-pointer p-3 rounded-2xl w-full`}>
             <Users/><span>Users</span></NavLink>
 
-             <NavLink to = '/dashboard/orders' className={({isActive})=>  `text-xl ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
+             <NavLink to = '/dashboard/orders' className={({isActive})=>  `text-lg ${isActive? "bg-pink-600 text-gray-200":"bg-transparent"}
                 flex items-center gap-2 font-bold cursor-pointer p-3 rounded-2xl w-full`}>
             <FaRegEdit/><span>Orders</span></NavLink>
         </div>
