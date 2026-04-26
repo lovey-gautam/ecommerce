cca620b97a74b6aa0bb56f18d2543d4419b26e85
lovey-gautam <loveygautam55@gmail.com>
2026-04-26 12:00:06 +0530

    Update SingleProduct.jsx

--- Frontend/src/pages/SingleProduct.jsx
+++ Frontend/src/pages/SingleProduct.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 const SingleProduct = () => {
-  const {id} = useParams();
+  const {id:productId} = useParams();
     const params = useParams()
     const productId = params.id;
     const {products} = useSelector(store=>store.product)
@@ -19,11 +19,15 @@ if (!product) {
   return <p>Product not found</p>;
 }
   return (
-    <div className='pt-20 py-10 max-w-7xl mx-auto'>
+    <div className='pt-20 py-10 max-w-7xl mx-auto px-4'>
       <Breadcrums product={product}/>
-      <div className='mt-10 grid grid-cols-2 items-start'>
+      <div className='mt-10 flex flex-col md:flex-row gap-8 items-start'>
+        <div  className="w-full md:w-1/2" >
         <ProductImg images={product.productImg} product={product}/>
+      </div>
+        <div  className="w-full md:w-1/2">
         <ProductDesc  product={product}/>
+        </div>
       </div>
     </div>
   )
