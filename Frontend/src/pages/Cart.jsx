7e728e49f7bf0e2d9cef024a5a14b6be957f94da
lovey-gautam <loveygautam55@gmail.com>
2026-05-03 14:30:47 +0530

    Update Cart.jsx

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -95,7 +95,7 @@ const loadCart = async()=>{
         cart?.items?.length >0 ? (
         <div className='max-w-7xl mx-auto'>
         <h1 className='text-2xl font-bold text-gray-800 mb-7'>Shopping Cart</h1>
-        <div className='max-w-7xl mx-auto flex gap-7'>
+        <div className='max-w-7xl mx-auto flex flex-col lg:flex-row gap-4 lg:gap-7'>
         <div className='flex flex-col gap-5 flex-1'>
           {cart?.items?.map((product,index)=>{
             return <Card key={index} >
@@ -121,7 +121,7 @@ const loadCart = async()=>{
             </Card>
           })}
           </div>
-        <div>
+        <div className='order-2 lg:order-none'>
           <Card className='w-full lg:w-[400px]'>
             <CardHeader>
               <CardTitle> Order Summary
