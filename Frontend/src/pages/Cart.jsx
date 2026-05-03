59acdbf5eb3f5d24828042d0c20fe4bb5a0f5cf9
lovey-gautam <loveygautam55@gmail.com>
2026-05-03 19:05:49 +0530

    Update Cart.jsx

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -201,8 +201,8 @@ const navigate = useNavigate()
 
       </div>
   
-    <div className='fixed bottom-0 left-0 right-0 p-3 bg-white border-t lg:hidden z-50'> 
-    <Button onClick={() => navigate('/address')} className='w-full bg-pink-600' >
+    <div className='fixed bottom-0 left-0 right-0 p-3 bg-white border-t  z-50'> 
+    <Button onClick={() => navigate('/address')} className='w-full bg-pink-600 mt-4 hidden lg:block' >
       PLACE ORDER 
     </Button> </div> 
     </div>
