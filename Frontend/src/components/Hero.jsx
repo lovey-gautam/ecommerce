d57e571a1628c3eda6826af23723165bf8715eb8
lovey-gautam <loveygautam55@gmail.com>
2026-05-04 12:48:05 +0530

    Update Hero.jsx

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Button } from './ui/button'
+import { useNavigate } from 'react-router-dom'
+
 const Hero = () => {
   return (
     <section className='bg-gradient-to-r mx-auto from-blue-600 to-purple-600 text-white py-10 md:py-16'>
@@ -9,7 +11,9 @@ const Hero = () => {
       <h1 className='text-3xl sm:text-4xl md:text-6xl pt-4 font-bold mb-4'>Latest Electronics at Best Prices</h1>  
       <p className='text-xl mb-6 text-blue-100'>Discover cutting-edge technology with unbeatable deals on smartphones, laptops and more.</p> 
       <div className='flex flex-col sm:flex-row gap-4'>
-        <Button className ='bg-white text-blue-600 hover:bg-gray-100'>Shop Now</Button>
+        <Button className ='bg-white text-blue-600 hover:bg-gray-100'              
+      onClick={() => navigate('/products')}
+     >Shop Now</Button>
         <Button variant='outline' className='border-white text-white hover:bg-white hover:text-blue-600 bg-transparent'>View Deals</Button>
         </div>   
             </div>
