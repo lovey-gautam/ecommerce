552e5969ef6b32e8adc74c969b0f4ae852e2e011
lovey-gautam <loveygautam55@gmail.com>
2026-05-04 23:54:03 +0530

    Update SingleProduct.jsx

--- Frontend/src/pages/SingleProduct.jsx
+++ Frontend/src/pages/SingleProduct.jsx
@@ -21,11 +21,11 @@ if (!product) {
   return (
     <div className='pt-20 py-10 max-w-7xl mx-auto px-4'>
       <Breadcrums product={product}/>
-      <div className='mt-10 flex flex-col md:flex-row gap-8 items-start'>
+      <div className='mt-6 flex flex-col md:flex-row gap-3 md:gap-8 items-start'>
         <div  className="w-full md:w-1/2" >
         <ProductImg images={product.productImg} product={product}/>
       </div>
-        <div  className="w-full pt-4 md:w-1/2">
+        <div  className="w-full md:w-1/2">
         <ProductDesc  product={product}/>
         </div>
       </div>
