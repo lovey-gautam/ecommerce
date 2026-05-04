8836b62f69a69f5f4ea72073e4da527dfa8a93af
lovey-gautam <loveygautam55@gmail.com>
2026-05-04 18:01:55 +0530

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -5,15 +5,27 @@ import Navbar from '@/components/Navbar';
 
 const Dashboard = () => {
   return (
-    <div className='flex'> 
-    <Sidebar/>
-        
-        <div className='flex-1'>
-          <Outlet/>
+    <div className="flex min-h-screen">
+
+      {/* Sidebar - hidden on mobile */}
+      <div className="hidden md:block">
+        <Sidebar />
+      </div>
+
+      {/* Main content */}
+      <div className="flex-1 flex flex-col w-full">
+
+        {/* Optional Navbar */}
+        <Navbar />
+
+        {/* Page content */}
+        <div className="p-3 md:p-6 w-full">
+          <Outlet />
         </div>
 
       </div>
-    
+
+    </div>
   );
 };
 
